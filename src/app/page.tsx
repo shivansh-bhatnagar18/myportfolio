02c70aa8ec6e4f01f3ff029aa87334aa00f8252a
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2024-05-16 00:17:42 +0530

    Some minor UI chnages in the top bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="h-[100vh] flex items-center justify-center bg-[#393A59]">
       <div className="flex flex-col max-w-[800px] w-full h-[500px] overflow-hidden rounded-[6px] border-[1px] border-gray-500 m-[20px] cursor-text">
-        <div className="flex items-center text-center flex-row h-[24px] w-full bg-[#424040] cursor-default py-[8px]">
+        <div className="flex items-center text-center flex-row h-[24px] w-full bg-[#424040] cursor-default px-[8px]">
           <div className="flex flex-row absolute">
             <div className="w-[12px] h-[12px] rounded-[50%] mr-[8px] cursor-pointer bg-[#f5544d]"></div>
             <div className="w-[12px] h-[12px] rounded-[50%] mr-[8px] cursor-pointer bg-[#fabd2f]"></div>
@@ -12,8 +12,8 @@ export default function Home() {
           </div>
           <div className="flex-1 text-center">
             <a href="https://github.com/shivansh-bhatnagar18" target="_blank" className="text-[#EEEEEE]">
-              <i className='fab fa-github'></i>
-              <h1 className="bg-[#9c9c9c] text-[13px]">github.com/shivansh-bhatnagar18</h1>
+              {/* <i className='fab fa-github'></i> */}
+              <h1 className="text-[#9c9c9c] text-[13px]">github.com/shivansh-bhatnagar18</h1>
             </a>
           </div>
         </div>
